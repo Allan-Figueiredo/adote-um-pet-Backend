f5a61179984b73282f2f17bf789c3824c83f687c
AllanFGR <115052566+Allan-Figueiredo@users.noreply.github.com>
2025-08-08 11:18:57 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+
+
+app.use(cors({ 
+  credentials: true, 
+  origin: 'http://localhost:3000',
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
+
+app.use(express.static('public'))
+
+
+const UserRoutes = require('./routes/UserRoutes')
+const PetRoutes = require('./routes/PetRoutes')
+
+
+app.use('/users', UserRoutes)
+app.use('/pets', PetRoutes)
+
+app.listen(5000)
